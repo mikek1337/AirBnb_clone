05277d3d12fecd00bcf29a07d858f59c67213093
mikek1337 <mikekegnlyew@gmail.com>
2022-11-22 22:24:02 +0300

    create_at and updated_at

--- models/base_model.py
+++ models/base_model.py
@@ -2,14 +2,17 @@
 """Base module."""
 
 import uuid
+from datetime import date
 
 """" BaseModel class for util purpose """
 
 
 class BaseModel:
     id = str(uuid.uuid4())
+    created_at = date.today()
+    updated_at = date.today()
 
 
 if __name__ == "__main__":
     base_model = BaseModel()
-    print(base_model.id)
+    print(base_model.created_at)
