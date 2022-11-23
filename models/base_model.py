d2c94cc0641889325d840a59f01811d29a103182
mikek1337 <mikekegnlyew@gmail.com>
2022-11-23 23:15:40 +0300

    need a test case

--- models/base_model.py
+++ models/base_model.py
@@ -1,22 +1,48 @@
 #!/usr/bin/env python3
-"""Base module."""
+
 
 import uuid
-from datetime import date
+from datetime import datetime
 
 """" BaseModel class for util purpose."""
 
 
 class BaseModel:
-    id = str(uuid.uuid4())
-    created_at = date.today()
-    updated_at = date.today()
+    """Base module class."""
+
+    def __init__(self, *args, **kwargs):
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        """Converts datetime to string time format."""
+        copy_dict = dict(self.__dict__)
+        copy_dict['__class__'] = self.__class__.__name__
+        copy_dict['updated_at'] = self.updated_at.strftime(
+            "%Y-%m-%dT%H:%M:%S.%f")
+        copy_dict['created_at'] = self.created_at.strftime(
+            "%Y-%m-%dT%H:%M:%S.%f")
+        return copy_dict
+
+    def save(self):
+        """Update update date."""
+        self.__dict__['updated_at'] = datetime.now()
 
     def __str__(self):
-        pass
-
+        """Return string format to print."""
+        return ("[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__))
 
 
 if __name__ == "__main__":
     base_model = BaseModel()
-   
+    base_model.name = "mikias"
+    base_model.my_number = 100
+
+    base_model.save()
+
+    dic_json = base_model.to_dict()
+    print(dic_json)
+    for key in dic_json.keys():
+        print("\t{}: ({}) - {}".format(key,
+              type(dic_json[key]), dic_json[key]))
--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -1,15 +1,21 @@
+"""Module test case."""
 import unittest
+
 from models.base_model import BaseModel
 from datetime import date
 
-"""unit test module for base model class"""
 
 class TestBaseModel(unittest.TestCase):
+    """Test class for base model."""
+
     def setUp(self):
+        """To setup for test case."""
         self.base_model = BaseModel()
 
     def test_created_at(self):
+        """Testing if date time is created on instances."""
         self.assertEqual(self.base_model.created_at, date.today())
+
 
 if __name__ == '__main__':
     unittest.main()
