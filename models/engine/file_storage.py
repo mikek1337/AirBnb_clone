84299ad6290147c5c2698491f073ca38053de94f
mikek1337 <mikekegnlyew@gmail.com>
2022-11-27 18:45:42 +0300

    console finished;

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,37 +1,45 @@
+#!/usr/bin/python3
 """File storage module."""
 
 import json
+import models
 
 
 class FileStorage:
     """Storage engine instance."""
 
     __file_path = "file.json"
-    __objects = dict()
+    __objects = {}
 
     def new(self, obj):
         """Assigns new object to __objects."""
         if obj != None:
             obj_key = ''
-            obj_key = obj['__class__']+'.'+obj['id']
-            self.__class__.__objects[obj_key] = obj
+            obj_key = str(obj.__class__.__name__) + "." + str(obj.id)
+            value = obj
+            self.__objects[obj_key] = value
 
     def all(self):
         """Returns all the objects saved."""
-        return self.__class__.__objects
+        return self.__objects
 
     def save(self):
         """Saves the new object to file"""
-        try:
-            with open(self.__file_path, "w", errors="ignore") as f:
-                json.dump(self.__class__.__objects, f)
-        except OSError as e:
-            pass
+        rep_obj = {}
+        for key, val in self.__objects.items():
+            rep_obj[key] = val.to_dict()
+        with open(FileStorage.__file_path, "w",  encoding="UTF-8") as f:
+            json.dump(rep_obj, f)
+       
 
     def reload(self):
         """Loads object from file."""
         try:
-            with open("file.json", "r", errors="ignore") as f:
-                self.__class__.__objects = json.load(f)
+            with open(FileStorage.__file_path, "r", errors="ignore") as f:
+                FileStorage.__objects = json.load(f)
+                for key, val in self.__objects.items():
+                    class_name = val['__class__']
+                    class_name = models.classes[class_name]
+                    FileStorage.__objects[key] = class_name(**val)
         except OSError as e:
             pass
